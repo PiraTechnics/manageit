7babc3f0ce41acb01e0197d255df722fa4e1ec26
PiraTechnics <devin1younge@gmail.com>
2023-02-06 17:32:15 -0800

    React connect to Django REST api

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'manageit',
     'corsheaders',
     'rest_framework',
+    'manageit',
 
 ]
 
